36389cd56b3ae40e6fdb94910d10cb3c0345be9c
grokgrep <matthew.p.sheridan4@gmail.com>
2016-03-22 20:40:55 -0400

    Finally working.
    Need to add handling for commas in card names.

--- getmagiccardprices.py
+++ getmagiccardprices.py
@@ -8,56 +8,57 @@
 # Copyright:   (c) Geoff 2012
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+
+
+# Matthew Sheridan
+# getmagiccardprices.py
+# 15 November 2015
+# Retrieve current price data for MTG cards by name.
+
 #!/usr/bin/env python
 
+from PySide import QtCore, QtGui, QtWebKit
 import csv
 import os
 import re
 import sys
 import time
 
-from lxml import html
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from PyQt4.QtWebKit import *
+# import PyQt4.QtGui
+# import PyQt4.QtCore
+# import PyQt4.QtWebKit
 
 # Renders page passed as "url"
-class Render(QWebPage):
-  def __init__(self, url):
-    self.app = QApplication(sys.argv)
-    QWebPage.__init__(self)
-    self.loadFinished.connect(self._loadFinished)
-    self.mainFrame().load(QUrl(url))
-    self.app.exec_()
+def render(url):
+    page = QtWebKit.QWebPage()
+    loop = QtCore.QEventLoop()
+    page.mainFrame().loadFinished.connect(loop.quit)
+    page.mainFrame().load(url)
+    loop.exec_()
+    return page.mainFrame().toHtml()
 
-  def _loadFinished(self, result):
-    self.frame = self.mainFrame()
-    self.app.quit()
+app = QtGui.QApplication(sys.argv)
 
 # Input and output file handling
-ifile  = open(os.getcwd() + "\\" + str(sys.argv[1]), "rb")
+ifile = open(os.getcwd() + "\\" + str(sys.argv[1]), "rb")
 ofile = open(os.getcwd() + "\\" + str(sys.argv[2]), "a")
 if os.stat(os.getcwd() + "\\" + str(sys.argv[2])).st_size < 1:
     print("Creating " + sys.argv[2])
-    toWrite = "NAME,QTY,LOW(ea.),MID(ea.),HI(ea.),LOW,MID,HI\n"
+    toWrite = "NAME;QTY;LOW(ea.);MID(ea.);HI(ea.);LOW;MID;HI\n"
     ofile.write(toWrite)
 else:
     print("Appending " + sys.argv[2])
 
 # CSV reader and regex for price search
-reader = csv.reader(ifile, delimiter=",")
+reader = csv.reader(ifile, delimiter=";")
 pattern = re.compile("TCGPPriceLow\".*\$(\d*.\d\d).*TCGPPriceMid.*\$(\d*.\d\d).*TCGPPriceHigh[^\$]*\$(\d*.\d\d)")
 
 for row in reader:
     # Query for exact card name, no quotes.
-    # e.g. "http://magiccards.info/query?q=!Bane of Bala Ged"
     url = "http://magiccards.info/query?q=!" + str(row[0])
+    result = render(url)
 
-    # Render page and converts to text
-    r = Render(url)
-    result = r.frame.toHtml().toAscii()
-
-    # Fetch prices and print to file
+    # Fetch prices and write
     prices = pattern.search(result)
     toWrite = row[0] + "," + row[1] + ","
     if prices:
@@ -66,7 +67,8 @@ for row in reader:
         toWrite = toWrite + ",,,,,"
     toWrite = toWrite + "\n"
     ofile.write(toWrite)
+    # print toWrite
 
     # Need to exit here or Qt craps out as soon as second row is hit
     # Why?
-    exit()
+    # app.exit()
