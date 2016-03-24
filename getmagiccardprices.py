acd8523ff7f3f39737d8c3f1fb43c1bf4ed765a4
grokgrep <matthew.p.sheridan4@gmail.com>
2016-03-24 01:21:32 -0400

    v0.1g
    Reads CSV with name and quantity of card. Scrapes for prices based on
    exact name match. Only returns first result / latest printing of card
    (see http://magiccards.info/search.html and
    http://magiccards.info/syntax.html).
    
    Depends:
    docopt, PySide
    
    New:
    Added usage, proper file handling.
    
    Todo:
    Add search by specific edition.
    Add formatting for deckstats.net input and output.
    Clean up documentation (Python style guide??).

--- getmagiccardprices.py
+++ getmagiccardprices.py
@@ -12,8 +12,11 @@
   getmagiccardprices.py [-h] INPUT OUTPUT
 
 Arguments:
-  INPUT   Input file. A list of exact card names and quantities, delimited by semicolon.
-  OUTPUT  Output file. A CSV formatted for Excel; will be appended if it already exists.
+  INPUT   Input file. A list of exact card names and quantities, delimited by
+          semicolon.
+  OUTPUT  Output file. Will be a CSV formatted for Excel and will contain the
+          list of card names; quantities; and low, mid, and high prices. This
+          file will be appended if it already exists.
 
 Options:
   -h --help  Show this help message.
@@ -24,10 +27,11 @@ Options:
 __authors__ = "Geoff, Matthew Sheridan"
 __credits__ = ["Geoff", "Matthew Sheridan"]
 __date__    = "23 March 2016"
-__version__ = "0.1f"
+__version__ = "0.1g"
 __status__  = "Development"
 
-OUTPUT_FIELDS  = ["NAME","QTY","LOW (ea.)","MID (ea.)","HI (ea.)","LOW","MID","HI"]
+OUTPUT_FIELDS  = ["NAME", "QTY", "LOW (ea.)", "MID (ea.)",
+                  "HI (ea.)", "LOW", "MID", "HI"]
 SEARCH_PATTERN = "TCGPPriceLow\".*\$(\d*.\d\d).*TCGPPriceMid.*\$(\d*.\d\d).*TCGPPriceHigh[^\$]*\$(\d*.\d\d)"
 
 import os
@@ -37,7 +41,8 @@ import re
 from docopt import docopt
 from PySide import QtCore, QtGui, QtWebKit
 
-# Pre:  path is an input file path. This file lists names and quantities, delimited by semicolon.
+# Pre:  path is an input file path. This file lists names and quantities,
+#       delimited by semicolon.
 # Post: Returns list of card names to search for and quantity of each.
 def read_cards(path):
     global count_total
@@ -50,9 +55,12 @@ def read_cards(path):
     return dat
 
 # Pre:  path is an output file path.
-#       fields is the list of field names to be printed at the top of the output file.
-#       output is the list of rows to output, each item of which corresponds to a field name.
-# Post: The file at path has either been created or appended with the rows of card names, quantities, and prices.
+#       fields is the list of field names to be printed at the top of the output
+#       file.
+#       output is the list of rows to output, each item of which corresponds to
+#       a field name.
+# Post: The file at path has either been created or appended with the rows of
+#       card names, quantities, and prices.
 def write_cards(path, fields, output):
     with open(path, "ab") as o:
         writer = csv.writer(o, dialect='excel')
@@ -103,12 +111,17 @@ def scrape(input_rows, pattern):
         prices = regex.search(result)
 
         # Display running progress.
-        sys.stdout.write("\rFetching... (" + str(counter) + "/" + str(count_total) + ")")
+        sys.stdout.write("\rFetching... (" + str(counter) + "/" +
+                         str(count_total) + ")")
         sys.stdout.flush()
 
         # If exact match was found, add its data to output.
         if prices:
-            dat.append([name, qty, prices.group(1), prices.group(2), prices.group(3), float(prices.group(1)) * qty, float(prices.group(2)) * qty, float(prices.group(3)) * qty])
+            dat.append([name, qty, prices.group(1),
+                        prices.group(2), prices.group(3),
+                        float(prices.group(1)) * qty,
+                        float(prices.group(2)) * qty,
+                        float(prices.group(3)) * qty])
             count_success += 1
         else:
             dat.append([name, qty])
@@ -117,9 +130,8 @@ def scrape(input_rows, pattern):
     app.exit()
     return dat
 
-if __name__ == "__main__":
-    # Parse arguments.
-    args = docopt(__doc__, help=True, version=__version__)
+def main(args):
+    # Filename and path handling.
     input_filename = str(args["INPUT"])
     output_filename = str(args["OUTPUT"])
     input_path = os.getcwd() + "\\" + str(input_filename)
@@ -127,12 +139,12 @@ if __name__ == "__main__":
     if not os.path.isfile(input_path):
         error(input_filename + " is not a valid filename.\n")
 
-    # Counters for summary and misc.
+    # Counters for summary.
     count_total = 0
     count_success = 0
     count_failed = 0
 
-    # Read in cards to search for, conduct the search, and write results to file.
+    # Read in cards to search for, conduct the search, and write out results.
     input_rows = read_cards(input_path)
     output_rows = scrape(input_rows, SEARCH_PATTERN)
     write_cards(output_path, OUTPUT_FIELDS, output_rows)
@@ -143,3 +155,8 @@ if __name__ == "__main__":
         print "Found:\t" + str(count_success) + " card(s)."
     if count_failed > 0:
         print "Missed:\t " + str(count_failed) + " card(s)."
+
+if __name__ == "__main__":
+    # Parse arguments and call main function.
+    args = docopt(__doc__, help=True, version=__version__)
+    main(args)
