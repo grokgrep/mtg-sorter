3fdf97d3fbc7c11ec040dec02b2c46edd85506c4
segfaultmagnet <matthew.p.sheridan4@gmail.com>
2016-03-24 18:25:01 -0400

    0.1i
    Added config file (conf.ini).

--- README.md
+++ README.md
@@ -3,18 +3,43 @@ MTG sorting machine experimentation.
 
 A collaborative effort by [@awesor](https://github.com/awesor), [@7thGate](https://github.com/7thGate), and [@segfaultmagnet](https://github.com/segfaultmagnet).
 
+### What It Does
+Reads in a list of card names and quantities, then scrapes for prices based on an exact name match. Currently only returns the latest printing of each card (see magiccards.info [search](http://magiccards.info/search.html) and [syntax](http://magiccards.info/syntax.html)).
+
     Usage:
-      getmagiccardprices.py [-hox] INPUT OUTPUT
+      getmagiccardprices.py [-hlo] INPUT OUTPUT
 
-    Arguments:
-      INPUT   Input file. A list of exact card names and quantities, delimited by
-              semicolon.
-      OUTPUT  Output file. Will be a CSV formatted for Excel and will contain the
-              list of card names; quantities; and low, mid, and high prices. This
-              file will be appended if it already exists.
+		Arguments:
+			INPUT   Input file. A list of exact card names and quantities, delimited by
+							semicolon. By default, this is exported from a deckstats.net
+							collection as a CSV file.
+			OUTPUT  Output file. Will be a CSV formatted for Excel and will contain the
+							list of card names; quantities; and low, mid, and high prices. This
+							file will be appended if it already exists.
 
-    Options:
-      -h --help  Show this help message.
-      -o         Overwrite OUTPUT file instead of appending.
-      -x         Use Excel-formatted input from deckstats export.
-      --version  Display program version number.
+		Options:
+			-h --help  Show this help message.
+			-l         Use a semicolon-delimited list of names and quantities instead of
+								 default deckstats formatting.
+			-o         Overwrite OUTPUT file instead of appending.
+			--version  Display program version number.
+
+### But Wait, There's More
+Added file handling, configuration file, and basic error checking.
+
+Input format is defaulted to deckstats collection-style formatting.
+### Still To Come
+Add search by specific edition.
+
+Add handling for multiple instances of the same card name (duplicates, editions, foils).
+
+Improve formatting for deckstats.net input (add: .dec deck format).
+
+Clean up documentation (what is Python style??).
+
+### Stuff You'll Need
+[ConfigObj](https://pypi.python.org/pypi/configobj/)
+
+[docopt](https://pypi.python.org/pypi/docopt/)
+
+[PySide](https://pypi.python.org/pypi/PySide/)
--- getmagiccardprices.py
+++ getmagiccardprices.py
@@ -9,19 +9,21 @@
 # Description:  Retrieve current price data for MTG cards by name.
 
 """Usage:
-  getmagiccardprices.py [-hox] INPUT OUTPUT
+  getmagiccardprices.py [-hlo] INPUT OUTPUT
 
 Arguments:
   INPUT   Input file. A list of exact card names and quantities, delimited by
-          semicolon.
+          semicolon. By default, this is exported from a deckstats.net
+          collection as a CSV file.
   OUTPUT  Output file. Will be a CSV formatted for Excel and will contain the
           list of card names; quantities; and low, mid, and high prices. This
           file will be appended if it already exists.
 
 Options:
   -h --help  Show this help message.
+  -l         Use a semicolon-delimited list of names and quantities instead of
+             default deckstats formatting.
   -o         Overwrite OUTPUT file instead of appending.
-  -x         Use Excel-formatted input from deckstats export.
   --version  Display program version number.
 
 """
@@ -29,49 +31,62 @@ Options:
 __authors__ = "Geoff, Matthew Sheridan"
 __credits__ = ["Geoff", "Matthew Sheridan"]
 __date__    = "23 March 2016"
-__version__ = "0.1h"
+__version__ = "0.1i"
 __status__  = "Development"
 
-INPUT_FIELDS   = ["amount", "card_name", "is_foil", "is_pinned", "set_id"]
-OUTPUT_FIELDS  = ["NAME", "QTY", "LOW (ea.)", "MID (ea.)",
-                  "HI (ea.)", "LOW", "MID", "HI"]
+CONFIG_FILENAME = "conf.ini"
 SEARCH_PATTERN = "TCGPPriceLow\".*\$(\d*.\d\d).*TCGPPriceMid.*\$(\d*.\d\d).*TCGPPriceHigh[^\$]*\$(\d*.\d\d)"
 
-ifile = None
-ofile = None
-count_total = 0
+READ_FORMATS  = ["deckstats", "list-semi"]
+# WRITE_FORMATS = ["excel"]
+
+ERROR_INVALID_FILENAME = " is not a valid filename!"
+ERROR_READ_FORMAT = "Unknown read format!"
+
+deckstats_fields = None
+output_fields    = None
+ifile            = None
+ofile            = None
+read_format      = None
+write_format     = None
+write_overwrite  = False
+count_total   = 0
 count_success = 0
-count_failed = 0
-read_mode = 0
-write_mode = 0
-write_overwrite = False
+count_failed  = 0
 
 import os
 import sys
 import csv
 import re
+from configobj import ConfigObj
 from docopt import docopt
 from PySide import QtCore, QtGui, QtWebKit
 
 # Pre:  path is an input file path. This file lists names and quantities,
 #       delimited by semicolon.
 # Post: Returns list of card names to search for and quantity of each.
-def read_cards(path):
+def read_cards(path, read_format):
     global count_total
-    global read_mode
     dat = []
     with open(path, "rb") as i:
-        if read_mode == 1:
+        # For deckstats-formatted input:
+        if read_format == READ_FORMATS[0]:
             read = csv.reader(i, dialect="excel")
-        else:
-            read = csv.reader(i, delimiter=";")
-
-        for r in read:
-            if read_mode == 1:
-                if r[0] != INPUT_FIELDS[0]:
+            for r in read:
+                # Skip header row.
+                if r[0] != deckstats_fields[0]:
                     dat.append([r[1], r[0]])
-            else:
+
+        # For semicolon-delimited list:
+        elif read_format == READ_FORMATS[1]:
+            read = csv.reader(i, delimiter=";")
+            for r in read:
                 dat.append(r)
+
+        # Any strange value for read_format should have already been caught...
+        else:
+            error(ERROR_READ_FORMAT, False)
+
     count_total = len(dat)
     return dat
 
@@ -82,13 +97,15 @@ def read_cards(path):
 #       a field name.
 # Post: The file at path has either been created or appended with the rows of
 #       card names, quantities, and prices.
-def write_cards(path, fields, output):
-    global write_overwrite
-    f_mode = "ab"
-    if write_overwrite:
-        f_mode = "wb"
-    with open(path, f_mode) as o:
-        writer = csv.writer(o, dialect='excel')
+def write_cards(path, write_format, overwrite, fields, output):
+    # write_format
+    if overwrite:
+        write_mode = "wb"
+    else:
+        write_mode = "ab"
+
+    with open(path, write_mode) as o:
+        writer = csv.writer(o, dialect="excel")
         # Add header line to output if file is empty.
         if os.stat(path).st_size < 1:
             output.insert(0, fields)
@@ -96,12 +113,21 @@ def write_cards(path, fields, output):
             writer.writerow(r)
 
 # Pre:  msg is the specific error message to pring.
-# Post: Prints error message, usage, and quits.
-def error(msg, print_usage=False):
+# Post: Prints error message, help, and quits.
+def error(msg, print_help):
     print msg
-    if print_usage:
+    if print_help:
+        print ""
         print str(__doc__)[:-2]
     exit(1)
+
+def print_summary(success, failed):
+    # Sum everything up!
+    print ""
+    if count_success > 0:
+        print "Found:\t" + str(count_success) + " card(s)."
+    if count_failed > 0:
+        print "Missed:\t " + str(count_failed) + " card(s)."
 
 # Pre:  url is URL of webpage to render.
 # Post: Returns HTML of rendered page.
@@ -157,35 +183,48 @@ def scrape(input_rows, pattern):
     return dat
 
 def main():
-    global ifile
-    global ofile
-    global read_mode
-    global write_mode
-    # Filename and path handling.
-    input_path = os.getcwd() + "\\" + str(ifile)
-    output_path = os.getcwd() + "\\" + str(ofile)
-    if not os.path.isfile(input_path):
-        error(ifile + " is not a valid filename.\n", True)
+    global input_path
+    global output_path
+    global read_format
+    global write_format
 
     # Read in cards to search for, conduct the search, and write out results.
-    input_rows = read_cards(input_path)
+    input_rows = read_cards(input_path, read_format)
     output_rows = scrape(input_rows, SEARCH_PATTERN)
-    write_cards(output_path, OUTPUT_FIELDS, output_rows)
-
-    # Sum everything up!
-    print ""
-    if count_success > 0:
-        print "Found:\t" + str(count_success) + " card(s)."
-    if count_failed > 0:
-        print "Missed:\t " + str(count_failed) + " card(s)."
+    write_cards(output_path, write_format, write_overwrite,
+                output_fields, output_rows)
+    print_summary(count_success, count_failed)
 
 if __name__ == "__main__":
     # Parse arguments and call main function.
     args = docopt(__doc__, help=True, version=__version__)
+
+    # Handle configuration issues.
+    config = ConfigObj(CONFIG_FILENAME)
+    config_prices = config["get_prices"]
+    deckstats_fields = config_prices["deckstats_fields"]
+    output_fields = config_prices["output_fields"]
+
+    # Handle file paths.
     ifile = str(args["INPUT"])
     ofile = str(args["OUTPUT"])
+    input_path = os.getcwd() + "\\" + str(ifile)
+    output_path = os.getcwd() + "\\" + str(ofile)
+    if not os.path.isfile(input_path):
+        error(ifile + ERROR_INVALID_FILENAME, True)
     if args["-o"]:
         write_overwrite = True
-    if args["-x"]:
-        read_mode = 1
+
+    # Handle formatting.
+    # Read formatting:
+    if args["-l"]:
+        read_format = READ_FORMATS[1]
+    else:
+        read_format = READ_FORMATS[0]
+    if not read_format in READ_FORMATS:
+        error(ERROR_READ_FORMAT, False)
+
+    # Write formatting:
+    # write_format = WRITE_FORMATS[0]
+
     main()
